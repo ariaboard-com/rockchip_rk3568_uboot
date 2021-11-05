eeeaf0a9bb1abf76a4f77c3e375cff6cbd81e58c
weicz <wcz@t-chip.com.cn>
2021-11-05 14:55:02 +0800

    rockchip: boot_mode: supoort multi media boot
    Signed-off-by: weicz <wcz@t-chip.com.cn>

--- include/boot_rkimg.h
+++ include/boot_rkimg.h
@@ -71,6 +71,7 @@ int get_bcb_recovery_msg(void);
 int rockchip_read_dtb_file(void *fdt_addr);
 int init_kernel_dtb(void);
 int fit_write_trusty_rollback_index(u32 trusty_index);
+int get_bootdev_type(void);
 
 #ifndef CONFIG_SPL_BUILD
 int rockchip_get_boot_mode(void);
