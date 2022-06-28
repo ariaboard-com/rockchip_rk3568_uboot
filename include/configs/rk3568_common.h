2017f8f3cde38b62da425ff67768d0426a27fd81
Kyosuke Nekoyashiki <supercatexpert@gmail.com>
2022-06-28 12:47:54 +0900

    Use different bootcmd in android u-boot.

--- include/configs/rk3568_common.h
+++ include/configs/rk3568_common.h
@@ -90,16 +90,29 @@
 	BOOTENV
 
 #undef RKIMG_BOOTCOMMAND
+
+
+#ifdef CONFIG_ROCKCHIP_ANDROID_BOOT_FIRST
 #define RKIMG_BOOTCOMMAND		\
 	"gpio clear 014;" \
 	"setenv bootargs_orig \"${bootargs}\";" \
 	"setenv bootargs \"${bootargs_orig} root=/dev/mmcblk1p2\";" \
 	"run bootcmd_mmc1;" \
-	"mmc dev ${devnum};" \
-	"setenv bootargs \"${bootargs_orig} root=/dev/mmcblk0p6\";" \
-	"boot_fit;"			\
+	"setenv bootargs \"${bootargs_orig}\";" \
+	"run bootcmd_mmc0;" \
 	"boot_android ${devtype} ${devnum};" \
 	"run distro_bootcmd;"
+#endif
+#else
+#define RKIMG_BOOTCOMMAND		\
+	"gpio clear 014;" \
+	"setenv bootargs_orig \"${bootargs}\";" \
+	"setenv bootargs \"${bootargs_orig} root=/dev/mmcblk1p2\";" \
+	"run bootcmd_mmc1;" \
+	"setenv bootargs \"${bootargs_orig} root=/dev/mmcblk0p6\";" \
+        "boot_fit;" \
+	"run distro_bootcmd;"
+#endif
 #endif
 
 /* rockchip ohci host driver */
